ed6b2d272b2a8079bd4c2c67805767cd2bafc713
Sebastian Pięta <44878991+KraftUnderscore@users.noreply.github.com>
2020-01-15 15:20:54 +0100

    fixes + comments

--- src/main/scala/Game/GameManager.scala
+++ src/main/scala/Game/GameManager.scala
@@ -5,23 +5,25 @@ import akka.actor.{Actor, ActorRef}
 import scala.util.Random
 
 class GameManager(val player1 : ActorRef, val player2 : ActorRef) extends Actor{
-  var playerOneTurn = true
-  val gameState : Array[Int] = Array.ofDim(GameManager.GameStateSize)
+  var playerOneTurn = true  //tura gracza
+  val gameState : Array[Int] = Array.ofDim(GameManager.GameStateSize) //wszystkie dolki wraz z bazami
   val r: Random.type = Random
-  var turnCounter : Int = 0
 
+  //ustawia wartosci dolkow na wartosci poczatkowe (i zeruje bazy)
   def resetGame():Unit = {
     for(x<-0 until GameManager.GameStateSize)
       if(x!=GameManager.PlayerOneBase && x!=GameManager.PlayerTwoBase) gameState(x) = GameManager.StartingValues
       else gameState(x) = 0
   }
 
+  //wyswietlenie planszy i wyslanie komunikatu do odpowiedniego gracza (ktorego jest teraz tura)
   def callPlayerMove():Unit = {
     GameManager.Interface.displayGameBoard(playerOneTurn, gameState)
     if(playerOneTurn) player1 ! Player.Move(playerOneTurn, gameState)
     else player2 ! Player.Move(playerOneTurn, gameState)
   }
 
+  //poczatek gry - reset wartosci i wylosowanie zaczynajacego gracza, po czym wezwanie jego ruchu
   def startGame():Unit = {
     resetGame()
     val nextPlr = Random.nextInt(2)
@@ -31,6 +33,7 @@ class GameManager(val player1 : ActorRef, val player2 : ActorRef) extends Actor{
     callPlayerMove()
   }
 
+  //sprawdzenie, czy dolki graczy maja kulki - jak nie to przegrana
   def checkIfLost:Boolean = {
     var sum = 0
     for(x<-0 until GameManager.PlayerOneBase)sum+=gameState(x)
@@ -41,24 +44,41 @@ class GameManager(val player1 : ActorRef, val player2 : ActorRef) extends Actor{
     else false
   }
 
+  //zarejestrowanie ruchu wykonanego przez gracza (komunikat od niego)
   def registerPlayerMove(index : Int) : Unit ={
     var _index = index
-    if(!playerOneTurn) _index = index + GameManager.PlayerOneBase + 1
-    val valueAtIndex = gameState(_index)
-    gameState(_index) = 0
-
+    if(!playerOneTurn) _index = index + GameManager.PlayerOneBase + 1 //jezeli gracz 2 to zmieniamy przedzial z 0-5 na 7-12
+    val valueAtIndex = gameState(_index) //ilosc kuleczek w dolku => ile musimy sie przesunac do przodu, dodajac po 1 do dolkow
+    gameState(_index) = 0 //zabieramy wszystkie kulki z dolka
+    //dodajemy po 1 kuleczce na kazdym miejscu po prawej do wartosci valueAtIndex
     for(x<- 1 to valueAtIndex)
       gameState((x+_index)%GameManager.GameStateSize) += 1
-
+    //sprawdzamy, czy gra sie nie konczy
     if(checkIfLost) {
+      //sumujemy do bazy graczy wszystkie dolki - gracz 1
+      for(x<-0 until GameManager.PlayerOneBase) {
+        gameState(GameManager.PlayerOneBase) += gameState(x)
+        gameState(x) = 0
+      }
+      // --||-- gracz 2
+      for(x<-GameManager.PlayerOneBase+1 until GameManager.PlayerTwoBase) {
+        gameState(GameManager.PlayerTwoBase) += gameState(x)
+        gameState(x) = 0
+      }
+      //wyswietlamy ostateczny wynik planszy i wybieramy wygranego
+      GameManager.Interface.displayGameBoard(playerOneTurn, gameState)
       playerOneTurn = gameState(GameManager.PlayerOneBase) > gameState(GameManager.PlayerTwoBase)
+      //ogloszenie wyniku, zamkniecie systemu aktorow
       GameManager.Interface.displayWinner(playerOneTurn)
       context.system.terminate()
     }else {
+      //sprawdzamy, czy kulka wyladowala na polu gracza, ktorego teraz jest tura => gra jeszcze raz, moze zabiera kulki przeciwnika
       if(playerOneTurn) {
-        if(_index+valueAtIndex > GameManager.PlayerOneBase) playerOneTurn = false
+        if(_index+valueAtIndex > GameManager.PlayerOneBase) playerOneTurn = false //sprawdzenie czy jego tura
+        //if(gameState((valueAtIndex+_index)%GameManager.GameStateSize) == 1)??? //TODO: ZABRAC Z DOLKA PRZECIWNIKA
       }else{
         if(_index+valueAtIndex > GameManager.PlayerTwoBase) playerOneTurn = true
+        //if(gameState((valueAtIndex+_index)%GameManager.GameStateSize) == 1)??? //TODO: ZABRAC Z DOLKA PRZECIWNIKA
       }
 
       callPlayerMove()
@@ -70,7 +90,7 @@ class GameManager(val player1 : ActorRef, val player2 : ActorRef) extends Actor{
     case GameManager.RegisterMove (mov) => registerPlayerMove(mov)
   }
 }
-
+//stale i komunikaty do GameManager
 object GameManager{
   case object Start
   case class RegisterMove(move : Int)
--- src/main/scala/Game/MainObj.scala
+++ src/main/scala/Game/MainObj.scala
@@ -5,7 +5,7 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 object MainObj extends App {
   val actorSystem = ActorSystem()
   val player1 : ActorRef = actorSystem.actorOf(Props(classOf[Player], new AlwaysLastControls))
-  val player2 : ActorRef = actorSystem.actorOf(Props(classOf[Player], new AlwaysFirstControls))
+  val player2 : ActorRef = actorSystem.actorOf(Props(classOf[Player], new RandomControls))
   val gameManager : ActorRef = actorSystem.actorOf(Props(classOf[GameManager], player1, player2))
   gameManager ! GameManager.Start
 }
